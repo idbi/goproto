9ac5bbf2dd4b7c439cb89ab90a7000766a4fb890
nickaguilarh <nickaguilarh@users.noreply.github.com>
2023-12-27 21:00:10 +0000

    feat: add integrations apis fix: namespaces [gsols/proto-apis@019675a03da52577d6a0c266642ed109c27ca311]

--- integrations/utils/v1/messages.pb.go
+++ integrations/utils/v1/messages.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.32.0
 // 	protoc        (unknown)
-// source: base/utils/v1/messages.proto
+// source: integrations/utils/v1/messages.proto
 
 package v1
 
@@ -34,7 +34,7 @@ type GetVersionRequest struct {
 func (x *GetVersionRequest) Reset() {
 	*x = GetVersionRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_base_utils_v1_messages_proto_msgTypes[0]
+		mi := &file_integrations_utils_v1_messages_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -47,7 +47,7 @@ func (x *GetVersionRequest) String() string {
 func (*GetVersionRequest) ProtoMessage() {}
 
 func (x *GetVersionRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_base_utils_v1_messages_proto_msgTypes[0]
+	mi := &file_integrations_utils_v1_messages_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -60,7 +60,7 @@ func (x *GetVersionRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use GetVersionRequest.ProtoReflect.Descriptor instead.
 func (*GetVersionRequest) Descriptor() ([]byte, []int) {
-	return file_base_utils_v1_messages_proto_rawDescGZIP(), []int{0}
+	return file_integrations_utils_v1_messages_proto_rawDescGZIP(), []int{0}
 }
 
 type GetVersionResponse struct {
@@ -74,7 +74,7 @@ type GetVersionResponse struct {
 func (x *GetVersionResponse) Reset() {
 	*x = GetVersionResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_base_utils_v1_messages_proto_msgTypes[1]
+		mi := &file_integrations_utils_v1_messages_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -87,7 +87,7 @@ func (x *GetVersionResponse) String() string {
 func (*GetVersionResponse) ProtoMessage() {}
 
 func (x *GetVersionResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_base_utils_v1_messages_proto_msgTypes[1]
+	mi := &file_integrations_utils_v1_messages_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -100,7 +100,7 @@ func (x *GetVersionResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use GetVersionResponse.ProtoReflect.Descriptor instead.
 func (*GetVersionResponse) Descriptor() ([]byte, []int) {
-	return file_base_utils_v1_messages_proto_rawDescGZIP(), []int{1}
+	return file_integrations_utils_v1_messages_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *GetVersionResponse) GetVersion() string {
@@ -110,12 +110,13 @@ func (x *GetVersionResponse) GetVersion() string {
 	return ""
 }
 
-var File_base_utils_v1_messages_proto protoreflect.FileDescriptor
+var File_integrations_utils_v1_messages_proto protoreflect.FileDescriptor
 
-var file_base_utils_v1_messages_proto_rawDesc = []byte{
-	0x0a, 0x1c, 0x62, 0x61, 0x73, 0x65, 0x2f, 0x75, 0x74, 0x69, 0x6c, 0x73, 0x2f, 0x76, 0x31, 0x2f,
-	0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x0d,
-	0x62, 0x61, 0x73, 0x65, 0x2e, 0x75, 0x74, 0x69, 0x6c, 0x73, 0x2e, 0x76, 0x31, 0x1a, 0x1c, 0x67,
+var file_integrations_utils_v1_messages_proto_rawDesc = []byte{
+	0x0a, 0x24, 0x69, 0x6e, 0x74, 0x65, 0x67, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x73, 0x2f, 0x75,
+	0x74, 0x69, 0x6c, 0x73, 0x2f, 0x76, 0x31, 0x2f, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x73,
+	0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x15, 0x69, 0x6e, 0x74, 0x65, 0x67, 0x72, 0x61, 0x74,
+	0x69, 0x6f, 0x6e, 0x73, 0x2e, 0x75, 0x74, 0x69, 0x6c, 0x73, 0x2e, 0x76, 0x31, 0x1a, 0x1c, 0x67,
 	0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x61, 0x6e, 0x6e, 0x6f, 0x74, 0x61,
 	0x74, 0x69, 0x6f, 0x6e, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x1a, 0x1f, 0x67, 0x6f, 0x6f,
 	0x67, 0x6c, 0x65, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x66, 0x69, 0x65, 0x6c, 0x64, 0x5f, 0x62, 0x65,
@@ -131,34 +132,36 @@ var file_base_utils_v1_messages_proto_rawDesc = []byte{
 	0x6e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x22, 0x2e, 0x0a, 0x12, 0x47, 0x65, 0x74, 0x56,
 	0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x18,
 	0x0a, 0x07, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52,
-	0x07, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x42, 0x6a, 0x5a, 0x29, 0x67, 0x69, 0x74, 0x68,
-	0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x69, 0x64, 0x62, 0x69, 0x2f, 0x67, 0x6f, 0x2d, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x62, 0x61, 0x73, 0x65, 0x2f, 0x75, 0x74, 0x69, 0x6c, 0x73, 0x2f,
-	0x76, 0x31, 0x3b, 0x76, 0x31, 0xca, 0x02, 0x18, 0x49, 0x44, 0x42, 0x49, 0x5c, 0x50, 0x72, 0x6f,
-	0x74, 0x6f, 0x5c, 0x42, 0x61, 0x73, 0x65, 0x5c, 0x55, 0x74, 0x69, 0x6c, 0x73, 0x5c, 0x76, 0x31,
-	0xe2, 0x02, 0x21, 0x49, 0x44, 0x42, 0x49, 0x5c, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x5c, 0x42, 0x61,
-	0x73, 0x65, 0x5c, 0x55, 0x74, 0x69, 0x6c, 0x73, 0x5c, 0x76, 0x31, 0x5c, 0x4d, 0x65, 0x74, 0x61,
-	0x64, 0x61, 0x74, 0x61, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x07, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x42, 0x82, 0x01, 0x5a, 0x31, 0x67, 0x69, 0x74,
+	0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x69, 0x64, 0x62, 0x69, 0x2f, 0x67, 0x6f, 0x2d,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x69, 0x6e, 0x74, 0x65, 0x67, 0x72, 0x61, 0x74, 0x69, 0x6f,
+	0x6e, 0x73, 0x2f, 0x75, 0x74, 0x69, 0x6c, 0x73, 0x2f, 0x76, 0x31, 0x3b, 0x76, 0x31, 0xca, 0x02,
+	0x20, 0x49, 0x44, 0x42, 0x49, 0x5c, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x5c, 0x49, 0x6e, 0x74, 0x65,
+	0x67, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x73, 0x5c, 0x55, 0x74, 0x69, 0x6c, 0x73, 0x5c, 0x76,
+	0x31, 0xe2, 0x02, 0x29, 0x49, 0x44, 0x42, 0x49, 0x5c, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x5c, 0x49,
+	0x6e, 0x74, 0x65, 0x67, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x73, 0x5c, 0x55, 0x74, 0x69, 0x6c,
+	0x73, 0x5c, 0x76, 0x31, 0x5c, 0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0x62, 0x06, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_base_utils_v1_messages_proto_rawDescOnce sync.Once
-	file_base_utils_v1_messages_proto_rawDescData = file_base_utils_v1_messages_proto_rawDesc
+	file_integrations_utils_v1_messages_proto_rawDescOnce sync.Once
+	file_integrations_utils_v1_messages_proto_rawDescData = file_integrations_utils_v1_messages_proto_rawDesc
 )
 
-func file_base_utils_v1_messages_proto_rawDescGZIP() []byte {
-	file_base_utils_v1_messages_proto_rawDescOnce.Do(func() {
-		file_base_utils_v1_messages_proto_rawDescData = protoimpl.X.CompressGZIP(file_base_utils_v1_messages_proto_rawDescData)
+func file_integrations_utils_v1_messages_proto_rawDescGZIP() []byte {
+	file_integrations_utils_v1_messages_proto_rawDescOnce.Do(func() {
+		file_integrations_utils_v1_messages_proto_rawDescData = protoimpl.X.CompressGZIP(file_integrations_utils_v1_messages_proto_rawDescData)
 	})
-	return file_base_utils_v1_messages_proto_rawDescData
+	return file_integrations_utils_v1_messages_proto_rawDescData
 }
 
-var file_base_utils_v1_messages_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_base_utils_v1_messages_proto_goTypes = []interface{}{
-	(*GetVersionRequest)(nil),  // 0: base.utils.v1.GetVersionRequest
-	(*GetVersionResponse)(nil), // 1: base.utils.v1.GetVersionResponse
+var file_integrations_utils_v1_messages_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_integrations_utils_v1_messages_proto_goTypes = []interface{}{
+	(*GetVersionRequest)(nil),  // 0: integrations.utils.v1.GetVersionRequest
+	(*GetVersionResponse)(nil), // 1: integrations.utils.v1.GetVersionResponse
 }
-var file_base_utils_v1_messages_proto_depIdxs = []int32{
+var file_integrations_utils_v1_messages_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -166,13 +169,13 @@ var file_base_utils_v1_messages_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_base_utils_v1_messages_proto_init() }
-func file_base_utils_v1_messages_proto_init() {
-	if File_base_utils_v1_messages_proto != nil {
+func init() { file_integrations_utils_v1_messages_proto_init() }
+func file_integrations_utils_v1_messages_proto_init() {
+	if File_integrations_utils_v1_messages_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_base_utils_v1_messages_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_integrations_utils_v1_messages_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*GetVersionRequest); i {
 			case 0:
 				return &v.state
@@ -184,7 +187,7 @@ func file_base_utils_v1_messages_proto_init() {
 				return nil
 			}
 		}
-		file_base_utils_v1_messages_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_integrations_utils_v1_messages_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*GetVersionResponse); i {
 			case 0:
 				return &v.state
@@ -201,18 +204,18 @@ func file_base_utils_v1_messages_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_base_utils_v1_messages_proto_rawDesc,
+			RawDescriptor: file_integrations_utils_v1_messages_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_base_utils_v1_messages_proto_goTypes,
-		DependencyIndexes: file_base_utils_v1_messages_proto_depIdxs,
-		MessageInfos:      file_base_utils_v1_messages_proto_msgTypes,
+		GoTypes:           file_integrations_utils_v1_messages_proto_goTypes,
+		DependencyIndexes: file_integrations_utils_v1_messages_proto_depIdxs,
+		MessageInfos:      file_integrations_utils_v1_messages_proto_msgTypes,
 	}.Build()
-	File_base_utils_v1_messages_proto = out.File
-	file_base_utils_v1_messages_proto_rawDesc = nil
-	file_base_utils_v1_messages_proto_goTypes = nil
-	file_base_utils_v1_messages_proto_depIdxs = nil
+	File_integrations_utils_v1_messages_proto = out.File
+	file_integrations_utils_v1_messages_proto_rawDesc = nil
+	file_integrations_utils_v1_messages_proto_goTypes = nil
+	file_integrations_utils_v1_messages_proto_depIdxs = nil
 }
